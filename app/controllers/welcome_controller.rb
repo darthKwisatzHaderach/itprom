f29430a814b4976be8a477581b2fd0d46290b120
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-02-11 17:53:43 +0500

    [IP-0005], [IP-0006] - Remove trash. Create bindings.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,13 @@
 class WelcomeController < ApplicationController
+  def index
+    @we = Articles.where(alias: 'we').first
+    @for_what = Articles.where(alias: 'for-what').first
+    @what_is_that = Articles.where(alias: 'what-is-that').first
+    @how_it_works = Articles.where(alias: 'how-it-works').first
+    @whats_new = Articles.where(alias: 'whats-new').first
+    @why = Articles.where(alias: 'why').first
+    @how_much = Articles.where(alias: 'how-much').first
+    @industrial_automation = Articles.where(alias: 'industrial-automation').first
+    @contacts = Articles.where(alias: 'contacts').first
+  end
 end
