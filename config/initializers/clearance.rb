c0254621a85712366056e475219a7a4aefe382a2
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-02-12 16:04:49 +0500

    [IP-0009] - Bugfixes. Erb to slim

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,6 @@
 Clearance.configure do |config|
   config.mailer_sender = "reply@example.com"
+  Clearance::PasswordsController.layout 'contact'
+  Clearance::SessionsController.layout 'contact'
+  Clearance::UsersController.layout 'contact'
 end
