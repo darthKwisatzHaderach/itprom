673681c5c100c4d28092102636dd486f620d7b76
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-02-13 15:26:05 +0500

    [IP-0008] - Bugfixes

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Alpha
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.autoload_paths << Rails.root.join('lib')
+
     config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,7 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.allow_sign_up = true
+  config.redirect_url = '/'
+  config.mailer_sender = 'reply@example.com'
   Clearance::PasswordsController.layout 'contact'
   Clearance::SessionsController.layout 'contact'
   Clearance::UsersController.layout 'contact'
